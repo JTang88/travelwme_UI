2164611e7d1b908b6cbd1601eb5ad953688f80d8
Erica H. Kim <EricaHKim@Ericas-MacBook-Pro.local>
2017-12-11 17:14:12 -0800

    [add] Add apollo client and store redux info

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-// import { createStore } from 'redux';
+import { createStore } from 'redux';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import { BrowserRouter } from 'react-router-dom';
+import allReducers from './reducers';
 import App from './components/App';
 
-// const store = createStore();
+const client = new ApolloClient({
+  link: new HttpLink({ uri: 'http://localhost:3001/graphql' }),
+  cache: new InMemoryCache(),
+});
+
+const store = createStore(allReducers);
 
 ReactDOM.render((
-  <Provider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>), document.getElementById('app'));
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </ApolloProvider>), document.getElementById('app'));
