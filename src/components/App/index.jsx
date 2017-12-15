52202930e390ce7d3a55e96d005dc9d1a3e1ff8c
Jerry Tang <jerrytang@Jerrys-MacBook-Pro.local>
2017-12-15 11:59:40 -0800

    [fix]:Auth- fix the routing issue, it now takes user who are bot loged in  to landing, and once it's loged in, it goes directly to home page

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,29 +1,21 @@
 import React from 'react';
 import { Switch, Route, Redirect, BrowserRouter } from 'react-router-dom';
-// import decode from 'jwt-decode';
-// import Auth from '../Auth';
+import decode from 'jwt-decode';
 import HomePage from '../HomePage';
 import Landing from '../LandingPage';
-import Login from '../Auth/Login';
+
 
 const checkAuth = () => {
-  // const token = localStorage.getItem('token');
-  // const refreshToekn = localStorage.getItem('refreshToken');
-  // if (!token || refreshToekn) {
-  //   return false;
-  // }
-  // try {
-  //   const { exp } = decode(refreshToekn);
-  //   if (exp < new Date().getTime() / 1000) {
-  //     return false;
-  //   }
-  // } catch (e) {
-  //   return false;
-  // }
-  // return true;
   const token = localStorage.getItem('token');
-
-  if (token === null) {
+  if (!token) {
+    return false;
+  }
+  try {
+    const { exp } = decode(token);
+    if (exp < new Date().getTime() / 1000) {
+      return false;
+    }
+  } catch (e) {
     return false;
   }
   return true;
@@ -34,26 +26,71 @@ const AuthRoute = ({ component: Component, ...rest }) => (
   <Route
     {...rest}
     render={props => (
-    checkAuth() ? (
+    checkAuth() ?
       <Component {...props} />
-    ) : (
+    :
       <Redirect to={{ pathname: '/landing' }} />
     )
-  )}
+  }
   />
 );
 
 function App() {
   return (
     <div className="app">
-      <BrowserRouter>
-        <Switch>
-          <Route path="/landing" component={Landing} />
-          <AuthRoute path="/homepage" component={HomePage} />
-        </Switch>
-      </BrowserRouter >
+      <Switch>
+        <Route exact path="/landing" component={Landing} />
+        <AuthRoute exact path="/" component={HomePage} />
+        <AuthRoute path="/homepage" component={HomePage} />
+      </Switch>
     </div>
   );
 }
 
 export default App;
+
+
+// Below method works..
+
+// const checkAuth = () => {
+//   const token = localStorage.getItem('token');
+//   if (!token) {
+//     return false;
+//   }
+//   try {
+//     const { exp } = decode(token);
+//     if (exp < new Date().getTime() / 1000) {
+//       return false;
+//     }
+//   } catch (e) {
+//     return false;
+//   }
+//   return true;
+// };
+
+
+// const AuthRoute = ({ component: Component, ...rest }) => (
+//   <Route
+//     {...rest}
+//     render={props => (
+//     checkAuth() ?
+//       <Component {...props} />
+//     :
+//       <Redirect to={{ pathname: '/landing' }} />
+//     )
+//   }
+//   />
+// );
+
+// function App() {
+//   return (
+//     <div className="app">
+//       <Switch>
+//         <Route path="/landing" component={Landing} />
+//         <AuthRoute path="/" component={HomePage} />
+//       </Switch>
+//     </div>
+//   );
+// }
+
+// export default App;
