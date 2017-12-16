49eb086f9cfc63822901e026722776fae5c86a95
Erica H. Kim <EricaHKim@Ericas-MacBook-Pro.local>
2017-12-16 14:18:36 -0800

    [add][fix] Update nav bar for trips and remove trips reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,5 @@
 import { combineReducers } from 'redux';
-<<<<<<< HEAD
-import reducerTrips from './reducerTrips';
 import authReducer from './authReducer';
-=======
->>>>>>> refs/remotes/origin/master
 import userTrips from './reducerTrips';
 import showTrip from './reducerShowTrip';
 import searchTrip from './reducerSearchTrip';
@@ -11,11 +7,7 @@ import userId from './reducerUserId';
 import tripCreator from './reducerTripCreator';
 
 const allReducers = combineReducers({
-<<<<<<< HEAD
-  trip: reducerTrips,
   auth: authReducer,
-=======
->>>>>>> refs/remotes/origin/master
   trips: userTrips,
   showtrip: showTrip,
   search: searchTrip,
