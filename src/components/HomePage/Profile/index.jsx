d0c7b4a22b342ee50d7f02e1e8f31f0f8b87ad1e
Jerry Tang <jerrytang@Jerrys-MacBook-Pro.local>
2017-12-16 15:15:38 -0800

    [feat]:Profile- start implementing Profile feature

--- src/components/HomePage/Profile/index.jsx
+++ src/components/HomePage/Profile/index.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 
-function Profile() {
-  return (
-    <div>
-      <h1>Profile</h1>
-    </div>
-  );
+const FakeUser = {
+
 }
 
+
+class Profile extends React.Component {
+  
+  render() {
+    return (
+      <div>
+        <h1>Profile</h1>
+      </div>
+    );
+  }
+}
+
+
 export default Profile;
+
+
+
+
+// create a fake redux obj. and use it. 
+// assume
+// updateUser(id: Int!, username: String!, email: String!, gender: String!, age: Int!, fitness: String!, relationship_status: String!): [Int!]!
