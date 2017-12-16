f4072ae6e9214aa8dbe0958e2ec27411d8c45467
Erica H. Kim <EricaHKim@Ericas-MacBook-Pro.local>
2017-12-16 13:29:16 -0800

    [fix] rebase merged successfully

--- src/reducers/reducerTrips.js
+++ src/reducers/reducerTrips.js
@@ -1,10 +1,6 @@
 export default function (state = [], action) {
   switch (action.type) {
-<<<<<<< HEAD
-    case 'ALL_TRIPS':
-=======
     case 'USER_TRIPS':
->>>>>>> refs/remotes/origin/master
       return action.payload;
   }
   return state;
