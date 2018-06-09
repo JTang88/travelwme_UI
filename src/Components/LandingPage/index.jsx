b13b5416121ab42705df86c26ba38b4907a00e0d
Jerry Tang <jerrytang@Jerrys-MacBook-Pro.local>
2018-06-08 22:37:40 -0700

    [Style]: LandingPage- except for logo landingPage is completed

--- src/Components/LandingPage/index.jsx
+++ src/Components/LandingPage/index.jsx
@@ -1,36 +1,59 @@
-import React from 'react';
-import {
-  Link,
-  Redirect,
-} from 'react-router-dom';
+import React, { Fragment } from 'react';
+import { Typography, Button, Grid } from '@material-ui/core';
+import { Link, Redirect,  } from 'react-router-dom';
 import checkAuth from '../../services/checkAuth';
+import './landing.css';
 
 const Landing = () => (
   checkAuth() ? <Redirect to={{ pathname: '/homepage' }} /> : 
-  <div className="landimg">
-    <div className="pos-f-t">
-      <div className="collapse" id="navbarToggleExternalContent">
-        <div className="bg-inverse p-4">
-          <Link href="/login" to="/login">
-            <h4 className="text-white">Login</h4>
-          </Link>   
-          <Link href="/sign" to="/sign"> 
-            <h4 className="text-white signbtt">Signup</h4>
-          </Link>  
-        </div>
-      </div>
-      <nav className="navbar navbar-inverse bg-inverse">
-        <button className="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon" />
-        </button>
-      </nav>
+  <div className="landing">
+    <div className="landing-nav">
+      <Button variant="text" size="large" color="inherit">
+          about
+      </Button>
+      <Button variant="text" size="large" color="inherit" >
+          blog
+      </Button>
+      <Button variant="text" size="large" color="inherit">
+          sign up
+      </Button>
+      <Button variant="text" size="large" color="inherit" >
+        log in
+      </Button>
     </div>
-    <h1 className="text-center title">
-            Travel With Me
-    </h1>
-    <div className="text-center desc">
-            Why travel alone?
-            Find awesome people to go on your next adventure!
+    <div className="clearfix" />
+    {/* <div>
+      <Link href="/login" to="/login">
+        <h4>Login</h4>
+      </Link>   
+      <Link href="/sign" to="/sign"> 
+        <h4>Signup</h4>
+      </Link>  
+    </div> */}
+    <div className="landing-text-container">
+      <Typography variant="display3" gutterBottom color="inherit">
+        <strong>
+          Going on a #Adventure <br />
+          Experience the World <br />
+          Making Friends of a Life Time
+        </strong>
+      </Typography>
+    </div>
+    <div className="center-button">
+      <div className="btn-full">
+        Log in
+      </div>
+      <div className="btn-ghost"> Sign up </div>
+      {/* <div>
+        <Button size="large" variant="outlined" color="inherit">
+          Sign Up
+        </Button>
+      </div>
+      <div>
+        <Button size="large" variant="outlined" color="inherit">
+          Log In
+        </Button>  
+      </div> */}
     </div>
   </div>
 );
--- src/Components/index.jsx
+++ src/Components/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import HomePage from './HomePage';
 import Landing from './LandingPage';
 import Login from './Auth/Login';
 import Signup from './Auth/Signup';
 import checkAuth from './../services/checkAuth';
 import ForgotPassowrd from './Auth/ForgotPassword';
+import 'typeface-roboto'
 
 const AuthRoute = ({ component: Component, ...rest }) => (
   <Route
@@ -23,14 +25,16 @@ const AuthRoute = ({ component: Component, ...rest }) => (
 function App() {
   return (
     <div className="app">
-      <Switch>
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/login/forgot" component={ForgotPassowrd} />
-        <Route path="/sign" component={Signup} />
-        <Route exact path="/" component={Landing} />
-        <Route path="/login/forgot" component={Login} />
-        <AuthRoute path="/homepage" component={HomePage} redirectTo="/login" />
-      </Switch>
+      <CssBaseline>
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/login/forgot" component={ForgotPassowrd} />
+          <Route path="/sign" component={Signup} />
+          <Route exact path="/" component={Landing} />
+          <Route path="/login/forgot" component={Login} />
+          <AuthRoute path="/homepage" component={HomePage} redirectTo="/login" />
+        </Switch>
+      </CssBaseline>
     </div>
   );
 }
